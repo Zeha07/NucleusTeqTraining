b8abb3954797c410233906ffeb6fd59f40863900
Sakshi <satshriyakalvaani@gmail.com>
2024-05-02 15:34:44 +0530

    Database connection trial

---
+++ DatabaseConnectionTrial/connection.py
@@ -0,0 +1,19 @@
+import mysql.connector  
+ # Creating the connection object     
+conn_obj = mysql.connector.connect(host="localhost", user="Python", passwd="PythonCon",database = "Python")  
+  # creating the cursor object    
+cur_obj = conn_obj.cursor()  
+print(conn_obj )
+try:
+#  dbs = cur_obj.execute("create table Employee(name varchar(20) not null, id int(20) not null primary key, salary float not null, Dept_id int not null)")  
+ dbs = cur_obj.execute("show tables")
+except:
+ cur_obj.rollback()
+
+
+for x in cur_obj:
+ print(x)
+
+# print(cur_obj )
+conn_obj.close()  
+  
