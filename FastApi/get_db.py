eb3058434fdf200f54a63c7615046f712d907211
Sakshi <satshriyakalvaani@gmail.com>
2024-05-04 18:22:42 +0530

    fastApi practice

--- FastApi/get_db.py
+++ FastApi/get_db.py
@@ -1,13 +1,13 @@
 import mysql.connector 
 
-# def db():
-try:
-    s = msc.connect(host ="localhost" , user = "Python" , passwd = "PythonCon" , database ="Python")
-    cur = msc.cursor()
+def db():
+ try:
+     s = msc.connect(host ="localhost" , user = "Python" , passwd = "PythonCon" , database ="Python")
+     cur = msc.cursor()
      
-except Exception as e:
-    print(e)
-else:
-    print("successfully connected")
-    # return cur
+ except Exception as e:
+     print(e)
+ else:
+     print("successfully connected")
+ return cur
 
---
+++ FastApiTutorial/fastpractice/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'data':{'Blog list'}} 
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data' : {'unpublished list'}}
+
+@app.get('/blog/{id}')
+def show(id : int):
+    return {'data' :id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comment(id:int):
+    return {'data' : {'1','2'}}
